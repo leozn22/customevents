4323dac594d6ca669d6ab05a9dbad5754c76dd07
camilo <camilo@da7dbaea-593a-a44b-b36b-087abf9a81ec>
2019-08-23 21:00:07 +0000

    Alterações ativar serviços site.
    git-svn-id: https://192.168.0.39/svn/Sankhya/trunk/snkcustomevents@1287 da7dbaea-593a-a44b-b36b-087abf9a81ec

--- src/main/java/br/com/goup/snkcustomevents/orders/ActiveService.java
+++ src/main/java/br/com/goup/snkcustomevents/orders/ActiveService.java
@@ -7,25 +7,62 @@ import br.com.sankhya.extensions.actionbutton.ContextoAcao;
 import br.com.sankhya.extensions.actionbutton.Registro;
 import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
 import br.com.sankhya.jape.core.JapeSession;
+import br.com.sankhya.jape.dao.JdbcWrapper;
+import br.com.sankhya.jape.event.ModifingFields;
 import br.com.sankhya.jape.event.PersistenceEvent;
 import br.com.sankhya.jape.event.TransactionContext;
+import br.com.sankhya.jape.sql.NativeSql;
 import br.com.sankhya.jape.vo.DynamicVO;
 
 import java.math.BigDecimal;
+import java.sql.ResultSet;
 
 
-public class ActiveService extends SnkIntegrationsApi implements EventoProgramavelJava,AcaoRotinaJava  {
+public class ActiveService extends SnkIntegrationsApi implements EventoProgramavelJava, AcaoRotinaJava  {
 
-	public void verifyIfNeedToActiveServicesByProduct(PersistenceEvent persistenceEvent) throws Exception {			
+	public void ativarServicosSite(PersistenceEvent persistenceEvent) throws Exception {
+
+		DynamicVO iteVO = (DynamicVO) persistenceEvent.getVo();
+
+		if (iteVO.asString("USOPROD").toUpperCase().equals("M")
+				&& iteVO.asBigDecimal("ATUALESTOQUE").compareTo(BigDecimal.valueOf(1)) == 0) {
+			String url = urlApi + "/service/products/active/" + iteVO.asBigDecimal("NUNOTA");
+			IntegrationApi.send(url, "", "POST");
+		}
+	}
+
+	public void verifyIfNeedToActiveServicesByProduct(PersistenceEvent persistenceEvent, boolean insert) throws Exception {
 		
 		//ModifingFields modifingFields = persistenceEvent.getModifingFields();
 		DynamicVO cabVO = (DynamicVO) persistenceEvent.getVo();
 		Boolean confirmando = (Boolean) JapeSession.getProperty("CabecalhoNota.confirmando.nota",false);
 
-		if( ( confirmando || "L".equals(cabVO.asString("STATUSNOTA")) ) && cabVO.asBigDecimal("CODTIPOPER").compareTo(BigDecimal.valueOf(2100)) == 0) {
-			String nunota = cabVO.getProperty("NUNOTA").toString();
-			String url = urlApi+"/service/products/active/"+nunota;
-			IntegrationApi.send(url,"", "POST");
+		if( ( confirmando || "L".equals(cabVO.asString("STATUSNOTA")) || insert)
+				&& cabVO.asString("TIPMOV").toUpperCase().equals("C")) {
+
+			JdbcWrapper jdbc = persistenceEvent.getJdbcWrapper();
+			NativeSql sql = new NativeSql(jdbc);
+			StringBuffer consulta = new StringBuffer();
+
+			consulta.append("    SELECT SEQUENCIA");
+			consulta.append("      FROM TGFITE ");
+			consulta.append("     WHERE NUNOTA = :NUNOTA");
+			consulta.append("       AND USOPROD = 'M'");
+//			consulta.append("       AND ATUALESTOQUE = 1");
+
+			sql.setNamedParameter("NUNOTA", cabVO.asBigDecimal("NUNOTA"));
+			ResultSet result = sql.executeQuery(consulta.toString());
+			if (true) {
+				throw new Exception("Nota: " + cabVO.asBigDecimal("NUNOTA") + "\n" + " confirmando: "
+						+ confirmando + " Status: " + cabVO.asString("STATUSNOTA")
+				+ " result: "  + result.next());
+			}
+
+			if (result.next()) {
+				String nunota = cabVO.getProperty("NUNOTA").toString();
+				String url = urlApi + "/service/products/active/" + nunota;
+				IntegrationApi.send(url, "", "POST");
+			}
 		}
 	}
 
@@ -38,13 +75,16 @@ public class ActiveService extends SnkIntegrationsApi implements EventoProgramav
 	@Override
 	public void afterInsert(PersistenceEvent arg0) throws Exception {
 		// TODO Auto-generated method stub
-
+		ativarServicosSite(arg0);
 	}
 
 	@Override
 	public void afterUpdate(PersistenceEvent arg0) throws Exception {
 		// TODO Auto-generated method stub
-		verifyIfNeedToActiveServicesByProduct(arg0);
+		ModifingFields modifingFields = arg0.getModifingFields();
+		if (modifingFields.isModifing("ATUALESTOQUE")) {
+			ativarServicosSite(arg0);
+		}
 	}
 
 	@Override
@@ -72,7 +112,7 @@ public class ActiveService extends SnkIntegrationsApi implements EventoProgramav
 	public void doAction(ContextoAcao contexto) throws Exception {
 		Registro[] linhas = contexto.getLinhas();
 		Registro linha = linhas[0];
-		
+
 		String tipoOperacao = linha.getCampo("CODTIPOPER").toString();
 		String statusNota = linha.getCampo("STATUSNOTA").toString();
 		String nunota = linha.getCampo("NUNOTA").toString();
