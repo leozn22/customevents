42082cbaddda725cfd9dcde1c9e492ae974f472d
camilo <camilo@da7dbaea-593a-a44b-b36b-087abf9a81ec>
2020-03-27 15:06:47 +0000

    Ajuste para pagamento Caixa.
    git-svn-id: https://192.168.0.39/svn/Sankhya/trunk/snkcustomevents@2467 da7dbaea-593a-a44b-b36b-087abf9a81ec

--- src/main/java/br/com/goup/snkcustomevents/financial/UpdateSql.java
+++ src/main/java/br/com/goup/snkcustomevents/financial/UpdateSql.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 
 import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
 import br.com.sankhya.jape.dao.JdbcWrapper;
+import br.com.sankhya.jape.event.ModifingFields;
 import br.com.sankhya.jape.event.PersistenceEvent;
 import br.com.sankhya.jape.event.TransactionContext;
 import br.com.sankhya.jape.sql.NativeSql;
@@ -17,7 +18,43 @@ public class UpdateSql extends SnkIntegrationsApi implements EventoProgramavelJa
 	//private int codUsuarioIntegracaoProducao = 0;
 	
 	public UpdateSql() {
-		//this.forceUrl("ProductionTest"); // Opções: LocalTest, ProductionTest, AllTest, Production
+		this.forceUrl("ProductionTest"); // Opções: LocalTest, ProductionTest, AllTest, Production
+	}
+
+	private String gerarJson(PersistenceEvent persistenceEvent) {
+
+		DynamicVO financialVO 		  = (DynamicVO) persistenceEvent.getVo();
+		ModifingFields modifingFields = persistenceEvent.getModifingFields();
+
+		String json = "{"
+				+ "\"financialId\": " + financialVO.asBigDecimal("NUFIN").toString() + ","
+				+ "\"companyId\": " + financialVO.asBigDecimal("CODEMP").toString() + ","
+				+ "\"noteId\": " + financialVO.asBigDecimal("NUNOTA").toString() + ","
+				+ "\"noteNumber\": " + financialVO.asBigDecimal("NUMNOTA").toString() + ","
+				+ "\"usuarioBaixaId\": " + (modifingFields.isModifing("CODUSUBAIXA")
+											? modifingFields.getNewValue("CODUSUBAIXA").toString()
+											: financialVO.asBigDecimal("CODUSUBAIXA").toString()) + ","
+
+				+ "\"typeTitleId\": " + (modifingFields.isModifing("CODTIPTIT")
+										? modifingFields.getNewValue("CODTIPTIT").toString()
+										: financialVO.asBigDecimal("CODTIPTIT").toString()) + ","
+
+				+ "\"paymentDate\": \"" + (modifingFields.isModifing("DHBAIXA")
+										? modifingFields.getNewValue("DHBAIXA").toString()
+										: financialVO.asBigDecimal("DHBAIXA").toString()) + "\","
+
+				+ "\"paidValue\": \"" + (modifingFields.isModifing("VLRBAIXA")
+										? modifingFields.getNewValue("VLRBAIXA").toString()
+										: financialVO.asBigDecimal("VLRBAIXA").toString()) + "\","
+
+				+ "\"receiptCard\": \"" +  (financialVO.getProperty("RECEBCARTAO") != null ? financialVO.asString("RECEBCARTAO") : "") + "\" ,"
+				+ "\"deadline\": \"" + (modifingFields.isModifing("DTPRAZO")
+						? modifingFields.getNewValue("DTPRAZO").toString()
+						: financialVO.getProperty("DTPRAZO")) + "\" "
+				+ "}";
+
+		return json;
+
 	}
 
 	private void financial(PersistenceEvent persistenceEvent) throws Exception {
@@ -30,15 +67,25 @@ public class UpdateSql extends SnkIntegrationsApi implements EventoProgramavelJa
 		//Se for n�o atualiza o financeiro nos bancos de dados
 		//if(codUsuarioBaixa != codUsuarioIntegracaoHomologacao)
 		//{
-			if(persistenceEvent.getEntityProperty("DHBAIXA") != null && financialVO.asInt("RECDESP")==1)
+			ModifingFields modifingFields = persistenceEvent.getModifingFields();
+			if(modifingFields.isModifing("DHBAIXA")
+					&& modifingFields.getNewValue("DHBAIXA") != null
+					&& financialVO.asInt("RECDESP") == 1
+					&& financialVO.asInt("CODTIPOPER") == 3117)
 			{
-				int negotiationType = this.getNegotiationType(persistenceEvent, financialVO.asInt("NUNOTA"));
-				
-				if(negotiationType >= 200 && negotiationType <= 213)
-				{
-					String url = this.urlApi+"/financial/sankhya/"+financialVO.asBigDecimal("NUFIN").toString();
-					IntegrationApi.send(url, "", "POST");
-				}
+//				String url  = this.urlApi+"/financial/sankhya/"+financialVO.asBigDecimal("NUFIN").toString() + "/" + modifingFields.getNewValue("CODUSUBAIXA").toString();
+				String json = this.gerarJson(persistenceEvent);
+				String url  = this.urlApi+"/financial/sankhya";
+
+				IntegrationApi.send(url, json, "POST");
+
+//				int negotiationType = this.getNegotiationType(persistenceEvent, financialVO.asInt("NUNOTA"));
+//
+//				if(negotiationType >= 200 && negotiationType <= 213)
+//				{
+//					String url = this.urlApi+"/financial/sankhya/"+financialVO.asBigDecimal("NUFIN").toString();
+//					IntegrationApi.send(url, "", "POST");
+//				}
 			}
 		//}//Fim IF usuarioIntegracao
 	}
@@ -75,7 +122,7 @@ public class UpdateSql extends SnkIntegrationsApi implements EventoProgramavelJa
 	@Override
 	public void afterInsert(PersistenceEvent arg0) throws Exception {
 		// TODO Auto-generated method stub
-		financial(arg0);
+//		financial(arg0);
 	}
 
 	@Override
