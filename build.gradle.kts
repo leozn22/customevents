ecb7f99acae420eb85551b8f60f4442646832e1d
a.marcelo <a.marcelo@da7dbaea-593a-a44b-b36b-087abf9a81ec>
2022-03-11 12:09:51 +0000

    Correção dependencias gradle
    git-svn-id: https://192.168.0.39/svn/Sankhya/branches/SankhyaCustomEvents/RefatoracaoGradle@4417 da7dbaea-593a-a44b-b36b-087abf9a81ec

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("br.com.lughconsultoria", "lugh-lib-annotation", lughlibVersion)
     annotationProcessor("br.com.lughconsultoria", "lugh-lib-processor", lughlibVersion)
 
-    implementation("br.com.sankhya.extensions:snkwext:1.0")
+    implementation("br.com.sankhya", "cuckoo", skwVersion)
 
     implementation("org.jdom", "jdom", "1.1.3")
     implementation("br.com.sankhya", "mge-modelcore", skwVersion)
