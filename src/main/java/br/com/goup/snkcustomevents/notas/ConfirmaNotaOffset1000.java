4723549ee5d0d9a728142fcb33a0def573ebc806
Marcelo Almeida <marcelo.almeida@oktz.com.br>
2022-05-23 11:12:37 -0300

    Task #39 - Incluir a rotina de refazer financeiro na confirmação das notas

--- src/main/java/br/com/goup/snkcustomevents/notas/ConfirmaNotaOffset1000.java
+++ src/main/java/br/com/goup/snkcustomevents/notas/ConfirmaNotaOffset1000.java
@@ -7,6 +7,7 @@ import br.com.sankhya.jape.sql.NativeSql;
 import br.com.sankhya.modelcore.auth.AuthenticationInfo;
 import br.com.sankhya.modelcore.comercial.BarramentoRegra;
 import br.com.sankhya.modelcore.comercial.CentralFaturamento;
+import br.com.sankhya.modelcore.comercial.CentralFinanceiro;
 import br.com.sankhya.modelcore.comercial.ConfirmacaoNotaHelper;
 import br.com.sankhya.modelcore.util.EntityFacadeFactory;
 import br.com.sankhya.modelcore.util.SPBeanUtils;
@@ -74,6 +75,22 @@ public class ConfirmaNotaOffset1000 implements ScheduledAction {
                     System.out.println("Acao Agendada - Confirma Nota Nro. Unico: " + nunota);
 
                     JapeSession.SessionHandle hnd = null;
+                    try {
+                        hnd = JapeSession.open();
+                        hnd.execWithTX(() -> {
+                            try {
+                                CentralFinanceiro financeiroUtils = new CentralFinanceiro();
+                                financeiroUtils.inicializaNota(nunota);
+                                financeiroUtils.refazerFinanceiro();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        });
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        JapeSession.close(hnd);
+                    }
                     try {
                         hnd = JapeSession.open();
                         hnd.execWithTX(() -> {
